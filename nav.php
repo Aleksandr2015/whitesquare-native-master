446ba1b5eacdb549a0cc14e76337f826f559699a
Aleksandr2015 <charleksvik@gmail.com>
2014-11-03 17:34:40 +0200

    динамически вывел меню

--- nav.php
+++ nav.php
@@ -1,3 +1,31 @@
+		<?php
+
+        $primary = array(
+            'theme_location'  => 'primary',
+            'menu'            => 'primary',
+            'container'       => 'nav',
+            'container_class' => 'navbar',
+            'container_id'    => 'primary_navbar',
+            'menu_class'      => 'menu',
+            'menu_id'         => '',
+            'echo'            => true,
+            'fallback_cb'     => 'wp_page_menu',
+            'before'          => '<div class="menu-item-outer">',
+            'after'           => '<div>',
+            'link_before'     => '<div class="menu-item-inner">',
+            'link_after'      => '</div>',
+            'items_wrap'      => '<ul class="top-menu">%3$s</ul>',
+            'depth'           => 0,
+            'walker'          => ''
+        );
+
+        wp_nav_menu( $primary );
+
+        ?>
+
+		
+<!--
+		
 		<nav>
 			<ul class="top-menu">
 				<li><a href="/home/">HOME</a></li>
@@ -10,3 +38,4 @@
 				<li><a href="/contact/">CONTACT</a></li>
 			</ul>
 		</nav>
+-->

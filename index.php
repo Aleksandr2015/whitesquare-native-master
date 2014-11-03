5d0835e4bd91b99ebdc377569e176b6423ada881
Aleksandr2015 <charleksvik@gmail.com>
2014-11-03 16:30:57 +0200

    Указал head и footer + разбил старницу...
    1) Добавил в <head> <?php wp_head(); ?>
    2) Добавил в подвал <?php wp_footer(); ?>
    3) создал файлы header.php и footer.php
    4) вынес в header.php и footer.php часть кода из index.php
    5) подключил header.php и footer.php в index.php через функции <?php
    get_header(); ?> и <?php get_footer(); ?>

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html>
-<head>
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<title>Whitesquare</title>
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/styles.css" type="text/css">
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,300" type="text/css">
-	<!--[if lt IE 9]>
-	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-</head>
-<body>
+<?php get_header(); ?>
 	<div id="wrapper">
 		<header>
 			<a href="/"><img src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="Whitesquare logo"></a>
@@ -116,42 +104,4 @@
 			</div>
 		</section>
 	</div>
-	<footer>
-		<div id="footer">
-			<div id="twitter">
-				<h3>TWITTER FEED</h3>
-				<time datetime="2012-10-23"><a href="#">23 oct</a></time>
-				<p>In ultricies pellentesque massa a porta. Aliquam ipsum enim, hendrerit ut porta nec, ullamcorper et nulla. In eget mi dui, sit amet scelerisque nunc. Aenean aug</p>
-			</div>
-			<div id="sitemap">
-				<h3>SITEMAP</h3>
-				<div>
-					<a href="/home/">Home</a>
-					<a href="/about/">About</a>
-					<a href="/services/">Services</a>
-				</div>
-				<div>
-					<a href="/partners/">Partners</a>
-					<a href="/customers/">Support</a>
-					<a href="/contact/">Contact</a>
-				</div>
-			</div>
-			<div id="social">
-				<h3>SOCIAL NETWORKS</h3>
-				<a href="http://twitter.com/" class="social-icon twitter"></a>
-				<a href="http://facebook.com/" class="social-icon facebook"></a>
-				<a href="http://plus.google.com/" class="social-icon google-plus"></a>
-				<a href="http://vimeo.com/" class="social-icon-small vimeo"></a>
-				<a href="http://youtube.com/" class="social-icon-small youtube"></a>
-				<a href="http://flickr.com/" class="social-icon-small flickr"></a>
-				<a href="http://instagram.com/" class="social-icon-small instagram"></a>
-				<a href="/rss/" class="social-icon-small rss"></a>
-			</div>
-			<div id="footer-logo">
-				<a href="/"><img src="<?php bloginfo('template_url'); ?>/images/footer-logo.png" alt="Whitesquare logo"></a>
-				<p>Copyright &copy; 2012 Whitesquare. A <a href="http://pcklab.com">pcklab</a> creation </p>
-			</div>
-		</div>
-	</footer>
-</body>
-</html>
+	<?php get_footer(); ?>
